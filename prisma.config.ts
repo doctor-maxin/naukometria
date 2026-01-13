5281ea962ac7f53a704b3caafc0d2066920bef6c
Mukhriddin <doctor-maxin@yandex.ru>
2026-01-13 11:02:41 +0300

    1705_b Добавление внешних таблиц

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,11 +2,17 @@ import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
+  experimental: {
+    externalTables: true,
+  },
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
   },
   datasource: {
     url: process.env['DATABASE_URL'],
+  },
+  tables: {
+    external: ['organizations', 'publications', 'persons', 'journals'],
   },
 });
